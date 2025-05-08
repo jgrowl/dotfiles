cd1ed0b7931bb7eeeef3b2f6f65a3a3934d1c0dc
Jon Rowlands <jonrowlands83@gmail.com>
2025-05-08 18:18:43 -0500

    Updates neovim install script to use new url

--- scripts/root/root-debian-unstable-neovim.sh
+++ scripts/root/root-debian-unstable-neovim.sh
@@ -1,3 +1,7 @@
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+export NEOVIM_FILE='nvim-linux-x86_64.tar.gz'
+export NEOVIM_URL="https://github.com/neovim/neovim/releases/latest/download/$NEOVIM_FILE"
+sh -c 'echo Downloading NeoVim from $NEOVIM_URL'
+
+sh -c 'curl -LO $NEOVIM_URL'
 rm -rf /opt/nvim
-tar -C /opt -xzf nvim-linux64.tar.gz
+sh -c 'tar -vC /opt -xzf $NEOVIM_FILE'
