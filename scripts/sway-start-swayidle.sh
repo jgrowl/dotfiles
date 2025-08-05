c14ecba2717dd32ae302abd0ff43c6104d4e8ab4
Jon Rowlands <jonrowlands83@gmail.com>
2025-08-05 08:45:27 -0500

    Adds swayidle script. It always seems better to do swayidle in a script as it does not work right when putting the command directly into sway config

--- scripts/sway-start-swayidle.sh
+++ scripts/sway-start-swayidle.sh
@@ -4,10 +4,12 @@
 # pgrep -af swayidle
 
 # 30 minutes: 1800
+# 1 hour: 3600
+# 2 hours: 7200
 
 swayidle -w \
-  timeout 900 'swaylock -f -c 000000' \
-  timeout 1800 'swaymsg "output * dpms off"' \
+  timeout 3600 'swaylock -f -c 000000' \
+  timeout 7200 'swaymsg "output * dpms off"' \
   resume 'swaymsg "output * dpms on"' \
   before-sleep 'swaylock -f -c 000000'
 
